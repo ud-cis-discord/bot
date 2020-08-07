c8be2e1c5e272fdf4a433f86117b78b9abd1051b
BenSegal855 <bensegal@udel.edu>
2020-08-06 21:27:46 -0400

    Added discstatus and watching commands

---
+++ src/commands/Information/Miscellaneous/discstatus.ts
@@ -0,0 +1,48 @@
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Message } from 'discord.js';
+import { newEmbed, formatDate } from '@utils/util';
+import { Colors } from '@lib/types/enums';
+const request = require('request');
+
+export default class extends SteveCommand {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['discordstatus', 'isdiscordbroke'],
+			cooldown: 60,
+			cooldownLevel: 'channel',
+			description: 'See the current status of Discord'
+		});
+
+	}
+
+	public async run(msg: KlasaMessage): Promise<Message> {
+
+		let url = 'https://srhpyqt94yxb.statuspage.io/api/v2/summary.json';
+		let options = {json: true};
+
+		request(url, options, (error, res, body) => {
+			if (error) {
+				return  'Failed to retireve status';
+			};
+
+			if (!error && res.statusCode == 200) {
+				// do something with JSON, using the 'body' variable
+				const embed = newEmbed()
+				.setTitle(body.status.description)
+				.setDescription('[Discord Status](https://status.discordapp.com)')
+				.addFields(body.components.map(component => ({
+					name: component.name,
+					value: component.status,
+					inline: true
+				})))
+				.setTimestamp()
+				.setFooter(`Last changed: ${formatDate(body.page.updated_at)}`)
+				.setColor(Colors.SpaceXBlue);
+
+				return msg.channel.send(embed);
+			};
+		});
+		return null;
+	}
+}
---
+++ src/commands/System/watching.ts
@@ -0,0 +1,25 @@
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { PermissionLevels } from '@lib/types/enums';
+import { CommandStore, KlasaMessage } from 'klasa';
+import { TextChannel, GuildChannel } from 'discord.js';
+
+export default class extends SteveCommand {
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			aliases: ['everywhere'],
+			description: 'I am everywhere',
+			runIn: ['text'],
+			permissionLevel: PermissionLevels.OWNER
+		});
+	}
+
+	public async run(msg: KlasaMessage) {
+        (msg.channel as GuildChannel).parent.children.array().forEach(channel => {
+            if(channel.type == 'text') {
+				(channel as TextChannel).send('Ben is everywhere');
+			}
+        });
+	}
+
+}
