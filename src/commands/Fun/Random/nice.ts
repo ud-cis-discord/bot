ae6b3ea470a033afa567b95c5045a54d213b5fa8
BenSegal855 <bensegal@udel.edu>
2020-08-11 20:31:49 -0400

    doubt and nice

---
+++ src/commands/Fun/Random/nice.ts
@@ -0,0 +1,19 @@
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { CommandStore, KlasaMessage } from 'klasa';
+import { Message } from 'discord.js';
+
+export default class extends SteveCommand {
+
+        public constructor(store: CommandStore, file: string[], directory: string) {
+                super(store, file, directory, {
+                        description: `Nice`,
+                        examples: ['Nice']
+                });
+        }
+
+        public async run(msg: KlasaMessage): Promise<Message> {
+                msg.react('👌');
+		return;
+        }
+
+}
