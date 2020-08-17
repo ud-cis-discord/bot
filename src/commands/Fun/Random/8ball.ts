c469cda90dd9235b9aeb41d204ad026bae413f94
BenSegal855 <bensegal@udel.edu>
2020-08-17 18:53:47 -0400

    Updated 8ball regex

--- src/commands/Fun/Random/8ball.ts
+++ src/commands/Fun/Random/8ball.ts
@@ -10,7 +10,7 @@ export default class extends SteveCommand {
 			description: 'Ask the 8ball a question and you shall get an answer.',
 			extendedHelp: 'This command requires you to put a question mark at the end of your question.',
 			examples: ['8ball will the jonathans ever stop being annoying?'],
-			usage: '<question:reg/^[A-Za-z0-9_ ]+\\?/>',
+			usage: '<question:reg/^[A-Za-z0-9_\'" ]+\\?/>',
 			helpUsage: 'question?'
 		});
 
