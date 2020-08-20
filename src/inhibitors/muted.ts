b467aa0c2e9efbe0c2d963e7349f8ca07da4c6b8
BenSegal855 <bensegal@udel.edu>
2020-08-20 02:40:51 -0400

    Added muted inhibitor

---
+++ src/inhibitors/muted.ts
@@ -0,0 +1,15 @@
+import { Inhibitor, InhibitorStore } from 'klasa';
+import { KlasaMessage } from 'klasa';
+import { GuildSettings } from '@lib/types/settings/GuildSettings';
+
+export default class extends Inhibitor {
+
+	public constructor(store: InhibitorStore, file: string[], directory: string) {
+		super(store, file, directory, { spamProtection: true });
+	}
+
+	public async run(msg: KlasaMessage): Promise<void> {
+		const muted = msg.guild.settings.get(GuildSettings.Roles.Muted);
+		if (msg.member.roles.cache.has(muted)) throw 'Muted perople cant use commands';
+	}
+}
