829d0b00ad418692cc9dc6966b446d1aee5f542f
BenSegal855 <bensegal@udel.edu>
2020-08-20 14:20:17 -0400

    Added role ping injection protection

---
+++ src/inhibitors/roleping.ts
@@ -0,0 +1,14 @@
+import { Inhibitor, InhibitorStore } from 'klasa';
+import { KlasaMessage } from 'klasa';
+
+export default class extends Inhibitor {
+
+	public constructor(store: InhibitorStore, file: string[], directory: string) {
+		super(store, file, directory, { spamProtection: true });
+	}
+
+	public async run(msg: KlasaMessage): Promise<void> {
+		if (!msg.member.isAdmin && msg.content.match(/<@&[0-9]{18}>/)) 
+			throw 'It looks like you are trying to inject a role ping. I\'m not going to let you do that';
+	}
+}
