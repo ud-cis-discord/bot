183e568d3b71f6feb0799b39f0938323cec438ac
BenSegal855 <bensegal@udel.edu>
2020-08-24 23:58:05 -0400

    rule changes

--- src/commands/Information/Server Info/rules.ts
+++ src/commands/Information/Server Info/rules.ts
@@ -19,29 +19,38 @@ export default class extends SteveCommand {
 
 	rules: string[] = [
 		'Be respectful, there are no dumb questions.',
-		'Your nickname should be your real name, especially if you want participation points!',
-		'Don\'t spam. Don\'t spam emojis, pictures, messages, pings, etc. Just don\'t spam.',
-		'Please keep to relevant channel topics, and if a Staff member or Admin asks you to move your conversation, listen.',
 		'We encourage you to help each other! However, saying  things like "It works on my machine" or "I don\'t have the problem" isn\'t helpful.',
-		'Keep your language clean- don\'t say something you wouldn\'t say to your professor.',
-		'Make sure to read the [#useful-info] channel! As the name implies, there is useful info in there.',
-		'Respect what the Staff and Admins say, they are in charge for a reason. If they tell you to stop, then stop.'
+		'Please keep to relevant channel topics, and if a Staff member or Admin asks you to move your conversation, listen.',
+		'Don\'t spam. Don\'t spam emojis, pictures, messages, pings, etc. Just don\'t spam.',
+		'Racist and discriminatory comments will not be tolerated. Comments like these will result in an instant ban, a report to Discord\'s Trust' +
+		' and Safety team, and potential University actions.',
+		'Keep your language appropriate- don\'t say something you wouldn\'t say to your professor.',
+		'Make sure to get yourself verified. Instructions on how to do so can be found on our website at https://ud-cis-discord.github.io/verify/',
+		'Respect what the Staff and Admins say, they are in charge for a reason. If they tell you to stop, then stop.',
+		'The UD Code of Conduct applies to all activity on this server. For more information refer to the following link:' +
+		' http://www1.udel.edu/stuguide/19-20/code.html',
+		'Failure to follow any of the rules in your first 24 hours on the server will result in an instant ban.'
 	];
 
 	async all(msg: KlasaMessage): Promise<Message> {
 		if (!msg.member.isAdmin) throw 'Sorry but you cant do that';
+
 		msg.delete();
+
 		let buff = '';
 		this.rules.forEach((rule, idx) => {
 			buff += `[${idx}] ${rule}\n`;
 		});
+
 		await msg.channel.send({ files: [{ attachment: './assets/images/rules.png', name: 'rules.png' }] });
+		await msg.channel.send('Check out our website! <https://ud-cis-discord.github.io/>');
 		return msg.sendCode('ini', buff);
 	}
 
 	async view(msg: KlasaMessage, ruleNum: number): Promise<Message> {
-		if (ruleNum >= this.rules.length) throw `There is no rule ${ruleNum}`;
-		return msg.channel.send(this.rules[ruleNum]);
+		if (this.rules[ruleNum] === undefined) throw 'Please provide a rule number.';
+		if (ruleNum >= this.rules.length) throw `There is no rule ${ruleNum}.`;
+		return msg.channel.send(`**Rule ${ruleNum}**\n${this.rules[ruleNum]}`);
 	}
 
 }
