82908924b2932eb91b8fc9460ad8920ce12d7120
BenSegal855 <bensegal@udel.edu>
2020-09-07 20:46:29 -0400

    Added getinvite command for moderators

---
+++ src/commands/Moderation/Utilities/getinvite.ts
@@ -0,0 +1,25 @@
+import { CommandStore, KlasaMessage } from 'klasa';
+import { SteveCommand } from '@lib/structures/commands/SteveCommand';
+import { Message } from 'discord.js';
+import { PermissionLevels } from '@lib/types/enums';
+
+export default class extends SteveCommand {
+
+	public constructor(store: CommandStore, file: string[], directory: string) {
+		super(store, file, directory, {
+			description: 'Generates an invite link to be given to students',
+			aliases: ['getlink', 'link'],
+			runIn: ['text'],
+			permissionLevel: PermissionLevels.MODERATOR
+		});
+	}
+
+	public async run(msg: KlasaMessage): Promise<Message | void> {
+		const invite = await msg.guild.systemChannel.createInvite({ maxAge: 24 * 60 * 60, maxUses: 1, unique: true });
+
+		return msg.author.send(`Here is the invite link you requested. It has one use and expires in 24 hours:\nhttps://discord.gg/${invite.code}`)
+			.then(() => { msg.channel.send('An invite link has been sent to your DM\'s'); })
+			.catch(() => { msg.channel.send('❌ | You have DMs disabled, I couldn\'t send you the link in DMs.'); });
+	}
+
+}
